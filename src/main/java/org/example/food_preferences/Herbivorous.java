b408f7e651c6f863920b9657536c02e826e826e6
Babin Daniil <113771449+goder1@users.noreply.github.com>
2024-11-16 16:35:18 +0300

    deleting few directories that randomly occured here despite being included into gitignore2

--- src/main/java/org/example/food_preferences/Herbivorous.java
+++
@@ -1,6 +0,0 @@
-package org.example.food_preferences;
-
-import org.example.animals.Animal;
-
-public abstract class Herbivorous extends Animal {
-}
--- src/main/java/org/example/food_preferences/Predator.java
+++
@@ -1,6 +0,0 @@
-package org.example.food_preferences;
-
-import org.example.animals.Animal;
-
-public abstract class Predator extends Animal {
-}
